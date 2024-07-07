c098d14552a62489eb322a9e022c63a89190a91d
rxd-r <jirathanayosyingthummakul@gmail.com>
2024-07-07 13:30:16 +0700

    Update cleaner.py

--- scraper/cleaner.py
+++ scraper/cleaner.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def extract_first_number(field):
     # Use regular expression to extract the first number from the field
-    match = re.match(r'^([\d.]+)', field)
+    match = re.match(r'^([\d\.]+)', field)
     if match:
         return match.group(1)
     else:
@@ -35,7 +35,7 @@ with open('cleaned_player_stats.csv', 'w', newline='', encoding='utf-8') as clea
                 elif idx == 7:
                     diff = line[7][:3]
                     new_line.append(diff)
-                    print(diff)
+                    #print(diff)
                 elif idx == 8:
                     kast = line[8][:3].strip("%")
                     new_line.append(kast)
@@ -43,7 +43,7 @@ with open('cleaned_player_stats.csv', 'w', newline='', encoding='utf-8') as clea
                     fk = line[11][0:3]
                     fd = line[12][0:3]
                     fk_fd = int(fk)-int(fd)
-                    print(fk_fd)
+                    #print(fk_fd)
                     #print(fk_fd_diff)
                     new_line.append(fk_fd)
                 elif idx == len(line)-1:
@@ -64,5 +64,86 @@ try:
     player_stats.drop('empty', inplace=True, axis=1)
 except KeyError:
     pass
-print(player_stats)
+#print(player_stats)
 player_stats.to_csv('cleaned_player_stats.csv', index=False)
+
+match_scores = []
+with open("cleaned_match_scores.csv", 'w') as cleaned_stats:
+    csvwriter = csv.writer(cleaned_stats)
+    csvwriter.writerow(["Win/Loss"])
+    with open("match_scores.csv", 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader)
+        for i in csvreader:
+            match_scores.append(i)
+
+import csv
+
+match_scores = []
+results = []
+with open("cleaned_match_scores.csv", 'w') as cleaned_stats:
+    csvwriter = csv.writer(cleaned_stats)
+    csvwriter.writerow(["Win/Loss"])
+    with open("match_scores.csv", 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader)
+        for i in csvreader:
+            match_scores.append(i)
+    #print(match_scores, "\n")
+    for idx, i in enumerate(match_scores):
+        if idx % 2 == 0:
+            team1, team2 = int(str(match_scores[idx]).strip("''[]")), int(str(match_scores[idx + 1]).strip("''[]"))
+            if team1 > team2:
+                team1 = ['win']
+                team2 = ['lose']
+            else:
+                team1 = ['lose']
+                team2 = ['win']
+            for i in range(0,5):
+                csvwriter.writerow(team1)
+            for j in range(0,5):
+                csvwriter.writerow(team2)
+
+results2 = []
+with open("combined_stats.csv", 'w') as combined:
+    csvwriter = csv.writer(combined)
+    csvwriter.writerow(['name', 'rating', 'acs', 'k', 'd', 'a', 'k-d', 'kast%', 'adr', 'hs%',
+                             'fk', 'fd', 'fd/fd/diff', 'org', 'result'])
+    with open("cleaned_player_stats.csv", 'r') as csvfile2:
+        csvreader2 = csv.reader(csvfile2)
+        next(csvreader2)
+        with open("cleaned_match_scores.csv", 'r') as csvfile3:
+            csvreader3 = csv.reader(csvfile3)
+            next(csvreader3)
+            for i in csvreader3:
+                results2.append(i)
+            for idx, line in enumerate(csvreader2):
+                try:
+                    result_to_be_added = (str(results2[idx]).strip("''[]"))
+                    line.append(result_to_be_added)
+                    csvwriter.writerow(line)
+                except IndexError:
+                    print("Mismatch between number of stats recorded")
+#average stats for teams
+with open("combined_team_stats.csv", "w") as csvfile2:
+    csvwriter = csv.writer(csvfile2)
+    csvwriter.writerow(
+        ["org", "result", "avg_rating", "avg_acs", "avg_kast%", "avg_adr", "avg_hs%"])
+    with open("combined_stats.csv", 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader)
+        ind_stats = []
+        for line in csvreader:
+            ind_stats.append(line)
+        for idx, record in enumerate(ind_stats):
+            if idx % 5 == 0:
+                new_line = []
+                new_line.append(record[13])
+                new_line.append(record[14])
+                for idx1, data in enumerate(record):
+                    if idx1 == 1 or idx1 == 2 or idx1 == 7 or idx1 == 8 or idx1 == 9:
+                        new_data = (float(ind_stats[idx][idx1]) + float(ind_stats[idx + 1][idx1]) + float(
+                            ind_stats[idx + 2][idx1]) + float(ind_stats[idx + 3][idx1]) + float(
+                            ind_stats[idx + 4][idx1])) / 5
+                        new_line.append(new_data)
+                csvwriter.writerow(new_line)
